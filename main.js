94e165985660e2ae21e67d395f893ca07bfadda5
Gbvnsz <souzagabrielvini@gmail.com>
2024-01-25 18:30:38 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log();
+document.documentElement(scrollDown);
+// Remova ou ajuste este código, se estiver adicionando um evento de clique à imagem
+document.getElementById('sua-imagem').addEventListener('click', function () {
+    // Lógica de redirecionamento ou ação associada ao clique
+});
+
+
